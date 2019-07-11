3b7d06ac2ab833a52f61b045be797056e0e41af8
Elias Gabriel <elias.gabrielfn12@gmail.com>
2019-07-11 08:34:58 -0400

    Added project
    Signed-off-by: Elias Gabriel <elias.gabrielfn12@gmail.com>

---
+++ RockPaperScissors/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RockPaperScissors
+//
+//  Created by Elias Gabriel dos Santos Correa on 11/07/19.
+//  Copyright © 2019 Elias Gabriel dos Santos Correa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
